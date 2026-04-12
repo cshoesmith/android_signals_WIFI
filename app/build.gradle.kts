cdfd27d7547f991ea0cc23850da6414381bc4463
cshoesmith <76083023+cshoesmith@users.noreply.github.com>
2026-04-12 20:07:22 +1000

    Bump versionCode to 5 to fix install downgrade error

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.example.androidsignals"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
+        versionCode = 5
         versionName = "1.0.0"
 
         vectorDrawables {
